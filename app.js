aa47f1b1867e82a44dcaa064571cc08af1c799f4
Tejeshreddy <s565694@nwmissouri.edu>
2023-11-08 17:24:41 -0600

    added files

--- app.js
+++ app.js
@@ -3,12 +3,50 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var Costume = require("./models/costume");
+require('dotenv').config();
+const connectionString = process.env.MONGO_CON
+mongoose = require('mongoose');
+
+mongoose.connect(connectionString, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('Connected to MongoDB'))
+  .catch(err => console.error('Error connecting to MongoDB: ', err));
+
+//Get the default connection
+var db = mongoose.connection;
+
+//Bind connection to error event
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.once("open", function () {
+  console.log("Connection to DB succeeded")
+});
+async function recreateDB() {
+  // Delete everything
+  await Costume.deleteMany();
+  let instance1 = new
+    Costume({
+      costume_type: "ghost", size: 'large',
+      cost: 15.4
+    });
+  instance1.save().then(doc => {
+    console.log("First object saved")
+  }
+  ).catch(err => {
+    console.error(err)
+  });
+}
+let reseed = true;
+if (reseed) { recreateDB(); }
+
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var dressRouter = require('./routes/dress');
+//var dressRouter = require('./routes/house');
 var boardRouter = require('./routes/board');
 var chooseRouter = require('./routes/choose');
+var resourceRouter = require('./routes/resource');
+const { connection } = require('mongoose');
+
 var app = express();
 
 // view engine setup
@@ -23,18 +61,19 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/dress', dressRouter);
+//app.use('/dress', dressRouter);
 app.use('/board', boardRouter);
 app.use('/choose', chooseRouter);
+app.use('/resource', resourceRouter);
 
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
--- controllers/costumeController.js
+++ controllers/costumeController.js
@@ -1,21 +1,28 @@
 var Costume = require('../models/costume');
 // List of all Costumes
-exports.costume_list = function(req, res) {
-res.send('NOT IMPLEMENTED: Costume list');
+exports.costume_list = async function (req, res) {
+    try {
+        theCostumes = await Costume.find();
+        res.send(theCostumes);
+    }
+    catch (err) {
+        res.status(500);
+        res.send(`{"error": ${err}}`);
+    }
 };
 // for a specific Costume.
-exports.costume_detail = function(req, res) {
-res.send('NOT IMPLEMENTED: Costume detail: ' + req.params.id);
+exports.costume_detail = function (req, res) {
+    res.send('NOT IMPLEMENTED: Costume detail: ' + req.params.id);
 };
 // Handle Costume create on POST.
-exports.costume_create_post = function(req, res) {
-res.send('NOT IMPLEMENTED: Costume create POST');
+exports.costume_create_post = function (req, res) {
+    res.send('NOT IMPLEMENTED: Costume create POST');
 };
 // Handle Costume delete form on DELETE.
-exports.costume_delete = function(req, res) {
-res.send('NOT IMPLEMENTED: Costume delete DELETE ' + req.params.id);
+exports.costume_delete = function (req, res) {
+    res.send('NOT IMPLEMENTED: Costume delete DELETE ' + req.params.id);
 };
 // Handle Costume update form on PUT.
-exports.costume_update_put = function(req, res) {
-res.send('NOT IMPLEMENTED: Costume update PUT' + req.params.id);
+exports.costume_update_put = function (req, res) {
+    res.send('NOT IMPLEMENTED: Costume update PUT' + req.params.id);
 };
