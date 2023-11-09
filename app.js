f1be78e8c2372332e10df5ee8c43df7c250222e4
Tejeshreddy <s565694@nwmissouri.edu>
2023-11-08 18:44:28 -0600

    modified

--- app.js
+++ app.js
@@ -1,86 +1,93 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var Costume = require("./models/costume");
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+const mongoose = require("mongoose");
+var resourceRouter = require('./routes/resource');
+var costumeRouter = require('./routes/costumes');
 require('dotenv').config();
 const connectionString = process.env.MONGO_CON
-mongoose = require('mongoose');
-
-mongoose.connect(connectionString, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log('Connected to MongoDB'))
-  .catch(err => console.error('Error connecting to MongoDB: ', err));
-
+mongoose.connect(connectionString, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.log("Error Connecting to MongoDB: ", err));
 //Get the default connection
 var db = mongoose.connection;
-
 //Bind connection to error event
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
-db.once("open", function () {
-  console.log("Connection to DB succeeded")
-});
+db.once("open", function () { console.log("Connection to DB succeeded") })
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
+var dressRouter = require('./routes/dress');
+var boardRouter = require('./routes/board');
+var chooseRouter = require("./routes/choose");
+var Costume = require("./models/costume");
 async function recreateDB() {
   // Delete everything
   await Costume.deleteMany();
-  let instance1 = new
-    Costume({
-      costume_type: "ghost", size: 'large',
-      cost: 15.4
+  let instance1 = new Costume(
+    {
+      costume_type: "Doremon",
+      size: 'Large',
+      cost: 25.4
     });
-  instance1.save().then(doc => {
-    console.log("First object saved")
-  }
-  ).catch(err => {
-    console.error(err)
-  });
+  let instance2 = new Costume(
+    {
+      costume_type: "Robo",
+      size: 'Small',
+      cost: 50.4
+    });
+  let instance3 = new Costume(
+    {
+      costume_type: "Superman",
+      size: 'Medium',
+      cost: 10.5
+    });
+  instance1.save()
+    .then(doc => { console.log("First object saved") })
+    .catch(err => { console.error(err) })
+  instance2.save()
+    .then(doc => { console.log("Second object saved") })
+    .catch(err => { console.error(err) })
+  instance3.save()
+    .then(doc => { console.log("Third object saved") })
+    .catch(err => { console.error(err) })
 }
 let reseed = true;
 if (reseed) { recreateDB(); }
-
-
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var dressRouter = require('./routes/dress');
-var boardRouter = require('./routes/board');
-var chooseRouter = require('./routes/choose');
-var resourceRouter = require('./routes/resource');
-const { connection } = require('mongoose');
-
 var app = express();
-
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
-
-app.use(logger('dev'));
+app.set("view engine", "pug");
+app.set("views", path.join(__dirname, "views"));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use(express.static(path.join(__dirname, "public")));
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
 app.use('/dress', dressRouter);
 app.use('/board', boardRouter);
-app.use('/choose', chooseRouter);
-app.use('/resource', resourceRouter);
-
-
+app.use("/choose", chooseRouter);
+app.use("/resource", resourceRouter);
+app.use('/costumes', costumeRouter);
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404));
 });
-
 // error handler
 app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
+  res.locals.error = req.app.get("env") === "development" ? err : {};
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
-
 module.exports = app;
+
+
+
--- controllers/costumeController.js
+++ controllers/costumeController.js
@@ -10,6 +10,20 @@ exports.costume_list = async function (req, res) {
         res.send(`{"error": ${err}}`);
     }
 };
+
+
+// VIEWS
+// Handle a show all view
+exports.costume_view_all_Page = async function (req, res) {
+    try {
+        theCostumes = await Costume.find();
+        res.render('costumes', { title: 'Costume Search Results', results: theCostumes });
+    }
+    catch (err) {
+        res.status(500);
+        res.send(`{"error": ${err}}`);
+    }
+};
 // for a specific Costume.
 exports.costume_detail = function (req, res) {
     res.send('NOT IMPLEMENTED: Costume detail: ' + req.params.id);
