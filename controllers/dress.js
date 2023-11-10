0754b59dfe16c70f44590a60becb7c3d411b3014
Tejeshreddy <s565694@nwmissouri.edu>
2023-11-10 02:06:30 -0600

    modified dress.js for assignment 12

--- controllers/dress.js
+++ controllers/dress.js
@@ -25,8 +25,16 @@ exports.dress_view_all_Page = async function (req, res) {
     }
 };
 // for a specific dress.
-exports.dress_detail = function (req, res) {
-    res.send('NOT IMPLEMENTED: dress detail: ' + req.params.id);
+// for a specific Costume.
+exports.dress_detail = async function (req, res) {
+    console.log("detail" + req.params.id)
+    try {
+        result = await dress.findById(req.params.id)
+        res.send(result)
+    } catch (error) {
+        res.status(500)
+        res.send(`{"error": document for id ${req.params.id} not found`);
+    }
 };
 // Handle derss create on POST.
 // Handle derss create on POST.
@@ -56,4 +64,24 @@ exports.dress_delete = function (req, res) {
 // Handle dress update form on PUT.
 exports.dress_update_put = function (req, res) {
     res.send('NOT IMPLEMENTED: dress update PUT' + req.params.id);
+};
+//Handle Costume update form on PUT.
+exports.dress_update_put = async function(req, res) {
+console.log(`update on id ${req.params.id} with body
+${JSON.stringify(req.body)}`)
+try {
+let toUpdate = await dress.findById( req.params.id)
+// Do updates of properties
+if(req.body.dress_type)
+toUpdate.dress_type = req.body.dress_type;
+if(req.body.cost) toUpdate.cost = req.body.cost;
+if(req.body.size) toUpdate.size = req.body.size;
+let result = await toUpdate.save();
+console.log("Sucess " + result)
+res.send(result)
+} catch (err) {
+res.status(500)
+res.send(`{"error": ${err}: Update for id ${req.params.id}
+failed`);
+}
 };
--- routes/dress.js
+++ routes/dress.js
@@ -1,6 +1,10 @@
 var express = require("express");
-const dress_controlers = require("../controllers/dress");
+const dress_controllers = require("../controllers/dress");
 var router = express.Router();
 // GET dress
-router.get("/", dress_controlers.dress_view_all_Page);
+router.get("/", dress_controllers.dress_view_all_Page);
 module.exports = router;
+// GET request for one dress.
+router.get('/dress/:id', dress_controllers.dress_detail);
+//put request for one dress
+router.put('/dress/:id', dress_controllers.dress_update_put)
