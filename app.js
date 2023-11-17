314fceda51a7e24ed87caa8804e2f3bb0128fcb2
Tejeshreddy <s565694@nwmissouri.edu>
2023-11-17 00:46:10 -0600

    modified app.js added account.js

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
 const mongoose = require("mongoose");
 var resourceRouter = require('./routes/resource');
 var dressRouter = require('./routes/dress');
@@ -19,6 +21,24 @@ var db = mongoose.connection;
 //Bind connection to error event
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 db.once("open", function () { console.log("Connection to DB succeeded") })
+passport.use(new LocalStrategy(
+  function(username, password, done) {
+  Account.findOne({ username: username })
+  .then(function (user){
+  if (err) { return done(err); }
+  if (!user) {
+  return done(null, false, { message: 'Incorrect username.' });
+  }
+  if (!user.validPassword(password)) {
+  return done(null, false, { message: 'Incorrect password.' });
+  }
+  return done(null, user);
+  })
+  .catch(function(err){
+  return done(err)
+  })
+  })
+  )
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
 var dressRouter = require('./routes/dress');
@@ -66,6 +86,13 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(require('express-session')({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: false
+  }));
+  app.use(passport.initialize());
+  app.use(passport.session());
 app.use(express.static(path.join(__dirname, "public")));
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
