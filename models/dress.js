7cee804d1f31f91c69c2c296c4bcdc409c4e8c2d
Tejeshreddy <s565694@nwmissouri.edu>
2023-11-29 01:29:06 -0600

    modified dress.js dress.pug dresscreate.pug

--- models/dress.js
+++ models/dress.js
@@ -1,8 +1,7 @@
 const mongoose = require("mongoose")
 const dressSchema = mongoose.Schema({
-    dress_type: String,
-    size: String,
-    cost: Number
+    dress_type: {type:String,required:true},
+    size: {type:String,required:true},
+    cost: {type:Number,required:true,min:0,max:500}
 })
-module.exports = mongoose.model("dress",
-dressSchema)
+module.exports = mongoose.model("dress",dressSchema)
